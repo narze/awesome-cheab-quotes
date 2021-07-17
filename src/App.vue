2363fb3c970ecb74feac7bce941effa97b7628e2
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-17 23:39:33 +0700

    chore: Replace twind with tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue"
+import HelloWorld from "./components/HelloWorld.vue"
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,10 @@
-<template>
-  <!-- <p>{{ entries[0] }}</p> -->
-  <p>{{ entry }}</p>
-</template>
-
 <script lang="ts">
-import { ref, defineComponent } from "vue";
+import { ref, defineComponent } from "vue"
 
 interface Entry {
-  body: string;
-  tags: string[];
-  id: number;
+  body: string
+  tags: string[]
+  id: number
 }
 
 export default defineComponent({
@@ -21,39 +16,45 @@ export default defineComponent({
     },
   },
   setup: () => {
-    const count = ref(0);
-    const entries = ref<Entry[]>([]);
-    const entry = ref<Entry>({} as Entry);
+    const count = ref(0)
+    const entries = ref<Entry[]>([])
+    const entry = ref<Entry>({} as Entry)
 
     const getEntries = async () => {
       const response = await fetch(
         "https://raw.githubusercontent.com/narze/awesome-cheab-quotes/main/README.md"
-      );
+      )
 
-      const readme = await response.text();
+      const readme = await response.text()
 
       entries.value = readme
         .split("\n")
         .filter((line) => line.startsWith("- "))
         .map((l) => l.slice(2))
         .map((l, idx) => {
-          const tagsTmp = l.match(/(\[(\w+(,\s)?)+\])/g);
-          const body = l.replace(/(\[(\w+(,\s)?)+\])/g, "").trimEnd();
-          const tags = tagsTmp![0].slice(1, -1).split(/,\s?/);
+          const tagsTmp = l.match(/(\[(\w+(,\s)?)+\])/g)
+          const body = l.replace(/(\[(\w+(,\s)?)+\])/g, "").trimEnd()
+          const tags = tagsTmp![0].slice(1, -1).split(/,\s?/)
 
-          return { tags, body, id: idx + 1 };
-        });
+          return { tags, body, id: idx + 1 }
+        })
 
-      entry.value = entries.value[~~(Math.random() * entries.value.length)];
-    };
+      entry.value = entries.value[~~(Math.random() * entries.value.length)]
+    }
 
-    return { count, entries, getEntries, entry };
+    return { count, entries, getEntries, entry }
   },
   mounted() {
-    this.getEntries();
+    this.getEntries()
   },
-});
+})
 </script>
+
+<template>
+  <h1 class="text-6xl">คำคมเฉียบๆ</h1>
+  <!-- <p>{{ entries[0] }}</p> -->
+  <p>{{ entry }}</p>
+</template>
 
 <style scoped>
 a {
